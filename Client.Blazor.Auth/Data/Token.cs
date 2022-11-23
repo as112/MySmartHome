2ea01bb1bbcf1ab28a75f1accc00f5b35d30ebda
d.mushynski <d.mushynski@yukonww.com>
2022-11-23 14:45:10 +0200

    updates

--- Client.Blazor.Auth/Data/Token.cs
+++ Client.Blazor.Auth/Data/Token.cs
@@ -1,10 +1,25 @@
-﻿namespace Client.Blazor.Auth.Data
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Client.Blazor.Auth.Data
 {
     public class Token
     {
         public string? access_token { get; set; }
         public string? username { get; set; }
 
-        public static Dictionary<string, string> tokens = new();
+        //public Dictionary<string, string> tokens = new();
+        //private readonly AuthenticationStateProvider _provider;
+
+        //public Token(AuthenticationStateProvider provider)
+        //{
+        //    _provider = provider;
+        //}
+
+        //public async Task<string> GetTokenAsync()
+        //{
+        //    var authState = await _provider.GetAuthenticationStateAsync();
+        //    var user = authState.User;
+        //    return tokens[user.FindFirst(c => c.Type == ClaimTypes.Email)?.Value!];
+        //}
     }
 }
---
+++ Client.Blazor.Auth/Data/TokenStorage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Client.Blazor.Auth.Data
+{
+    public class TokenStorage
+    {
+        public List<Token> tokens { get; set; } = new List<Token>();
+
+        public string GetTokenByEmail(string email) => tokens.FirstOrDefault(s => s.username == email)?.access_token ?? "";
+    }
+}
